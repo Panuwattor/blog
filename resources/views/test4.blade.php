ed37e665cdae69f6905b5f55e715c745ab3a6ee0
panuwat penpanit <panuwat.taweechai@gmail.com>
2017-06-22 19:24:18 +0700

    ต่อ

---
+++ resources/views/test4.blade.php
@@ -0,0 +1,107 @@
+<!doctype html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Panuwat Penpanit</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+            .title1 {
+                font-size: 50px;
+                color: #0000ff;
+                padding: 0 25px;
+                font-size: 50px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @if (Auth::check())
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ url('/login') }}">Login</a>
+                        <a href="{{ url('/register') }}">Register</a>
+                    @endif
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    สามารถติดต่อเราได้ที่ 0831274231
+                </div>
+                <div class="title1 m-b-md">
+                    <a href=""="http://www.taweechai.com">ข้อมูลเพิ่มเติม**คลิ๊ก**</a>
+                </div>
+                <div class="links">
+                    <a href="/1">เราคือใคร</a>
+                    <a href="/2">เราทำอะไร</a>
+                    <a href="/3">ทีมงานเรา</a>
+                    <a href="/5">แผนที่</a>
+                    <a href="/">หน้าหลัก</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
